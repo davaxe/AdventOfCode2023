ac599a77b3b63abb6d7a78e730d5cd38649188e0
David Axelsson <davax320@student.liu.se>
2023-11-30 14:56:58 +0100

    Some more preparations

---
+++ template_src/part1.rs
@@ -0,0 +1,18 @@
+#[allow(dead_code)]
+
+fn task(_input: &str) -> Option<String> {
+    todo!("Implement the task here");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_task() {
+        let input = "";
+        assert!(task(input).is_some());
+        assert_eq!(task(input).unwrap(), "");
+    }
+}
+
