9b72c8b388fcdf197e6579dd932f49192c2b31cf
David Axelsson <davax320@student.liu.se>
2023-12-02 10:50:54 +0100

    Added day02

---
+++ day02/src/main.rs
@@ -0,0 +1,14 @@
+// Change day01 to the correct day
+use day02::{part1, part2};
+
+fn main() {
+    // Output results for both parts
+    println!(
+        "Part 1:\n  {}",
+        part1::task(include_str!("../part1-input.txt")).unwrap_or("No solution found".to_string())
+    );
+    println!(
+        "Part 2:\n  {}",
+        part2::task(include_str!("../part2-input.txt")).unwrap_or("No solution found".to_string())
+    );
+}
