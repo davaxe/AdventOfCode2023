969cbe815831e7f88dbc58175b71807b3b2b1c95
David Axelsson <davax320@student.liu.se>
2023-12-04 20:20:51 +0100

    Preparation day 5

---
+++ day05/src/main.rs
@@ -0,0 +1,12 @@
+use day05::{part1, part2};
+fn main() {
+    // Output results for both parts
+    println!(
+        "Part 1:\n  {}",
+        part1::task(include_str!("../part1-input.txt")).unwrap_or("No solution found".to_string())
+    );
+    println!(
+        "Part 2:\n  {}",
+        part2::task(include_str!("../part2-input.txt")).unwrap_or("No solution found".to_string())
+    );
+}
---
+++ day05/src/part1.rs
@@ -0,0 +1,15 @@
+pub fn task(_input: &str) -> Option<String> {
+    todo!("Implement the task here");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_task() {
+        let input = include_str!("../part1-example.txt");
+        assert!(task(input).is_some());
+        assert_eq!(task(input).unwrap(), "");
+    }
+}
