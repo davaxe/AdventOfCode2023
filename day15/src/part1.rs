5497320368d5f28cebda4bc86d40279ba4a5a063
David Axelsson <davax320@student.liu.se>
2023-12-15 14:56:57 +0100

    Complete with day 15

--- day15/src/part1.rs
+++ day15/src/part1.rs
@@ -1,5 +1,14 @@
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    Some(
+        input
+            .split(',')
+            .map(|s| {
+                s.bytes()
+                    .fold(0u32, |acc, b| ((acc + (b as u32)) * 17u32) % 256u32)
+            })
+            .sum::<u32>()
+            .to_string(),
+    )
 }
 
 #[cfg(test)]
--- day15/src/part2.rs
+++ day15/src/part2.rs
@@ -1,5 +1,67 @@
-pub fn task(_input: &str) -> Option<String> {
-    None
+use std::collections::HashMap;
+
+use itertools::Itertools;
+
+pub fn task(input: &str) -> Option<String> {
+    let mut boxes: HashMap<u32, Vec<(&str, u32)>> = HashMap::new();
+
+    input.split(',').for_each(|s| {
+        let (box_nr, idx) = s
+            .bytes()
+            .enumerate()
+            .take_while(|(_, c)| *c != b'=' && *c != b'-')
+            .fold((0u32, 0), |(acc, _), (i, b)| {
+                ((((acc + (b as u32)) * 17u32) % 256u32), i)
+            });
+
+        let label = &s[..idx + 1];
+        let operator = s.as_bytes()[idx + 1];
+
+        match operator {
+            b'=' => {
+                let focal_length = s[idx + 2..]
+                    .chars()
+                    .take_while(char::is_ascii_digit)
+                    .collect::<String>()
+                    .parse::<u32>()
+                    .unwrap();
+
+                // Modify if exists, otherwise insert new
+                boxes
+                    .entry(box_nr)
+                    .and_modify(|v| {
+                        if let Some((idx, _)) = v.iter().find_position(|(l, _)| *l == label) {
+                            v[idx] = (label, focal_length);
+                        } else {
+                            v.push((label, focal_length));
+                        }
+                    })
+                    .or_insert(vec![(label, focal_length)]);
+            }
+            b'-' => {
+                boxes.entry(box_nr).and_modify(|v| {
+                    if let Some((idx, _)) = v.iter().find_position(|(l, _)| *l == label) {
+                        v.remove(idx);
+                    }
+                });
+            }
+            _ => panic!("Invalid operator"),
+        }
+    });
+
+    // Calculate the total focusing power
+    Some(
+        boxes
+            .iter()
+            .map(|(box_nr, seq)| {
+                seq.iter()
+                    .enumerate()
+                    .map(|(i, (_, focal_length))| (1 + box_nr) * (i as u32 + 1) * focal_length)
+                    .sum::<u32>()
+            })
+            .sum::<u32>()
+            .to_string(),
+    )
 }
 
 #[cfg(test)]
@@ -11,6 +73,6 @@ mod tests {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "145");
     }
 }
